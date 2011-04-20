82047f219d5d57c674708239b85b44de626debc9
Blake Taylor <blakefrost@gmail.com>
2011-04-19 22:40:31 -0400

    Add expire and create http object once.

--- app.rb
+++ app.rb
@@ -3,11 +3,13 @@ require 'sinatra'
 require 'net/http'
 require "net/https"
 
+@@http = Net::HTTP.new('gist.github.com', 443)
+@@http.use_ssl = true
+@@http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
 get '/:gist/:file' do
-  @http = Net::HTTP.new('gist.github.com', 443)
-  @http.use_ssl = true
-  @http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  @http.start do |http|
+  expires 300
+  @@http.start do |http|
     req = Net::HTTP::Get.new("/raw/#{params[:gist]}/#{params[:file]}")
     response = http.request(req)
     response.body

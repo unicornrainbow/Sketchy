5bff24fd5e78f7966d12b54b1f93a7d78de4cdbe
Blake Taylor <blakefrost@gmail.com>
2011-04-19 22:06:15 -0400

    PAss file name in the url

--- app.rb
+++ app.rb
@@ -3,12 +3,12 @@ require 'sinatra'
 require 'net/http'
 require "net/https"
 
-get '/:gist' do
+get '/:gist/:file' do
   @http = Net::HTTP.new('gist.github.com', 443)
   @http.use_ssl = true
   @http.verify_mode = OpenSSL::SSL::VERIFY_NONE
   @http.start do |http|
-    req = Net::HTTP::Get.new("/raw/#{params[:gist]}/gistfile1.html")
+    req = Net::HTTP::Get.new("/raw/#{params[:gist]}/#{params[:file]}")
     response = http.request(req)
     response.body
   end
